3f979ceb7a2bdd41a6933a4dab561acfeec8ac98
Moayad Ali <108472902+muaiiad@users.noreply.github.com>
2024-04-22 00:09:17 +0200

    merge

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include <iostream>
 #include <string>
 #include <fstream> 
@@ -9,6 +10,9 @@ const float TILE_WIDTH = 32;
 const float FRICTION = 0.91f;
 const float GRAVITY = 2200.0f;
 const float TERMINAL_VELOCITY = 500.0f; //maximum vertical velocity
+
+
+
 
 int sign(const int& num) {
     return (num > 0 ? 1 : -1);
@@ -27,19 +31,33 @@ struct Animation {
 
 struct Tile {
     Sprite sprite;
+    Sprite coin;
     bool isSolid = false;
     bool isCoin = false;
+    bool isPowerup = false;
 };
 
 struct Level {
     int width = 128;
     int height = 32;
+
     Clock timer;
+    int countdown_timer = 120;
+
     std::string map_string;
     Texture spritesheet;
+    Texture feather;
+    Texture txCoin;
     Tile* tiles = nullptr; //dynamic array
     Texture backgroundImage;
     Sprite background;
+
+    Text score_text;
+    Text timer_text;
+
+
+    SoundBuffer coinBuffer;
+    Sound coinSound;
 };
 
 struct Player {
@@ -48,8 +66,11 @@ struct Player {
     RectangleShape debugger; //red square that shows the hitbox of the player which is smaller than the actual sprite
 
     Animation anim[3];
+    Animation wingsAnimation;
+    Sprite wings;
     int animationState = 0; // 0 for idle, 1 for running, 2 for jumping
 
+    int score = 0;
     Vector2f velocity;
     Vector2f acceleration;
     float maxVelocity = 300.0f;
@@ -64,6 +85,7 @@ struct Game {
     View camera;
     Level map;
     Player player;
+    Font font;
 };
 
 
@@ -78,12 +100,13 @@ void updatePlayer(Player& player, Level& level, float elapsed);
 void updatePlayerAnimation(Player& player);
 void collisionX(Player& player, Level& level);
 void collisionY(Player& player, Level& level);
+void drawPlayer(Game& game);
 
 void initAnimation(Animation& anim, int width, int height, int frameCount);
 void updateAnimation(Animation& anim);
 
 
-void initLevel(Level& level);
+void initLevel(Level& level, Font &font);
 void loadLevelFile(Level& level);
 void updateLevel(Game& game);
 void drawLevel(Game& game);
@@ -94,9 +117,12 @@ int main()
     Game game;
     initGame(game);
 
+    
+
     Clock clock;
 
     while (game.window.isOpen()) {
+
         updateGame(game, clock.restart().asSeconds());
         game.window.clear(Color::Black);
         drawGame(game);
@@ -114,8 +140,11 @@ void initGame(Game& game) {
     game.camera.setCenter(Vector2f(350.f, 300.f));
     game.camera.setSize(Vector2f(1056, 594));
 
+    game.font.loadFromFile("./assets/font.ttf");
+    game.font.setSmooth(false);
+
     initPlayer(game.player);
-    initLevel(game.map);
+    initLevel(game.map,game.font);
 }
 
 void updateGame(Game& game, float elapsed) {
@@ -161,8 +190,8 @@ void drawGame(Game& game) {
     if (game.map.tiles != nullptr) {
         drawLevel(game);
     }
-    //game.window.draw(game.player.debugger);
-    game.window.draw(game.player.body);
+    
+    drawPlayer(game);
     game.window.display();
 }
 
@@ -181,6 +210,17 @@ void initPlayer(Player& player) {
     player.anim[2].switchTime = 0.5f;
     initAnimation(player.anim[2], 33, 32, 2);
 
+
+    //wings  
+    player.wingsAnimation.spritesheet.loadFromFile("./assets/player/wings.png");
+    player.wingsAnimation.switchTime = 0.25f;
+    initAnimation(player.wingsAnimation, 16, 16, 2);
+    player.wings.setTexture(player.wingsAnimation.spritesheet);
+    player.wings.setTextureRect(player.wingsAnimation.currentSprite);
+    player.wings.setPosition(player.hitbox.getPosition());
+    player.wings.setOrigin(8, 8);
+    player.wings.setScale(2, 2);
+
     player.body.setTexture(player.anim[0].spritesheet);
     player.body.setTextureRect(player.anim[0].currentSprite);
     player.body.setScale(2, 2);
@@ -188,7 +228,7 @@ void initPlayer(Player& player) {
     player.hitbox = FloatRect(player.body.getPosition(), Vector2f(TILE_WIDTH, TILE_WIDTH));
 
     player.debugger = RectangleShape(Vector2f(TILE_WIDTH, TILE_WIDTH));
-    player.debugger.setFillColor(Color::Red);
+    player.debugger.setFillColor(Color::White);
 
     player.acceleration.x = 50;
 
@@ -201,9 +241,12 @@ void updatePlayer(Player& player, Level& level, float elapsed) {
     movePlayer(player, level, elapsed);
     updatePlayerAnimation(player);
 
-    //player.debugger.setPosition(player.hitbox.left,player.hitbox.top);
+    player.debugger.setPosition(round(player.hitbox.left),round(player.hitbox.top-250));
     player.body.setTexture(player.anim[player.animationState].spritesheet);
     player.body.setTextureRect(player.anim[player.animationState].currentSprite);
+    player.wings.setTextureRect(player.wingsAnimation.currentSprite);
+
+
 }
 
 void collisionX(Player& player, Level& level) {
@@ -213,7 +256,7 @@ void collisionX(Player& player, Level& level) {
         player.hitbox.left = 0;
     }
         
-
+    
     int left_tile = player.hitbox.getPosition().x / TILE_WIDTH;
     int right_tile = left_tile + 1;
     int vertical_pos = player.hitbox.getPosition().y / TILE_WIDTH;
@@ -231,8 +274,18 @@ void collisionX(Player& player, Level& level) {
 
             }
             
+            if (currentTile.isPowerup && player.hitbox.intersects(currentTile.sprite.getGlobalBounds())) {
+                player.canFly = true;
+                currentTile.isPowerup = false;
+            }
 
-
+            if (currentTile.isCoin && player.hitbox.intersects(currentTile.coin.getGlobalBounds())) {
+                currentTile.isCoin = false;
+                player.score++;
+                level.coinBuffer.loadFromFile("./assets/audio/coin.wav");
+                level.coinSound.setBuffer(level.coinBuffer);
+                level.coinSound.play();
+            }
 
         }
     }
@@ -247,7 +300,6 @@ void collisionY(Player& player, Level& level) { // no head collisions yet
     if (player.hitbox.top < 0)
         player.hitbox.top = 0;
 
-
     for (int j = horizontal_pos; j <= horizontal_pos + 1; j++) {
         Tile &currentTile = level.tiles[j + below_tile * level.width];
 
@@ -261,18 +313,31 @@ void collisionY(Player& player, Level& level) { // no head collisions yet
     }
 }
 
+void drawPlayer(Game& game) {
+    if (game.player.canFly)
+        game.window.draw(game.player.wings);
+    
+    game.window.draw(game.player.body);
+    //game.window.draw(game.player.debugger);
+}
+
 void movePlayer(Player& player, Level& level, float elapsed) {
 
     if (Keyboard::isKeyPressed(Keyboard::Scan::D)) {
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 3; i++) {
             player.anim[i].flipped = 0;
+        }
+            player.wingsAnimation.flipped = 0;
 
         player.velocity.x += player.acceleration.x;
     }
 
     if (Keyboard::isKeyPressed(Keyboard::Scan::A)) {
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 3; i++){
             player.anim[i].flipped = 1;
+        }
+            player.wingsAnimation.flipped = 1;
+
 
         player.velocity.x += player.acceleration.x * -1;
     }
@@ -298,6 +363,7 @@ void movePlayer(Player& player, Level& level, float elapsed) {
     collisionX(player, level);
     collisionY(player, level);
     player.body.setPosition(player.hitbox.left - 16, player.hitbox.top - 32);
+    player.wings.setPosition(player.hitbox.left + (player.wingsAnimation.flipped * 32), player.hitbox.top);
 }
 
 void updatePlayerAnimation(Player& player) {
@@ -308,6 +374,8 @@ void updatePlayerAnimation(Player& player) {
     if (abs(player.velocity.x) > 0 && player.velocity.y == 0)
         player.animationState = 1;
 
+
+    updateAnimation(player.wingsAnimation);
     updateAnimation(player.anim[player.animationState]);
 
     if (player.velocity.y < 0) {
@@ -324,7 +392,6 @@ void updatePlayerAnimation(Player& player) {
 
 
 }
-
 
 
 void initAnimation(Animation& anim, int width, int height, int frameCount) {
@@ -355,15 +422,25 @@ void updateAnimation(Animation& anim) {
 
 
 
-void initLevel(Level& level) {
+void initLevel(Level& level,Font &font) {
     level.spritesheet.loadFromFile("./assets/level/level_tileset.png");
+    level.feather.loadFromFile("./assets/level/feather.png");
     level.backgroundImage.loadFromFile("./assets/level/back.png");
+    level.txCoin.loadFromFile("./assets/level/gem.png");
     loadLevelFile(level);
 
     level.background.setTexture(level.backgroundImage);
     level.background.setColor(Color(230, 230, 230, 255));
     level.background.setScale(3, 3);
     level.background.setOrigin(192, 120); // set origin to center instead of top left
+
+    level.score_text.setFont(font);
+    level.score_text.setCharacterSize(48);
+    level.score_text.setFillColor(Color::White);
+
+    level.timer_text.setFont(font);
+    level.timer_text.setPosition(0, 0);
+    level.timer_text.setCharacterSize(48);
 
     level.tiles = new Tile[level.height * level.width];
 
@@ -373,6 +450,9 @@ void initLevel(Level& level) {
             level.tiles[j + i * level.width].sprite.setPosition(j * TILE_WIDTH, i * TILE_WIDTH);
             level.tiles[j + i * level.width].sprite.setTexture(level.spritesheet);
             level.tiles[j + i * level.width].sprite.setScale(2, 2);
+
+            level.tiles[j + i * level.width].coin.setPosition(j * TILE_WIDTH, i * TILE_WIDTH);
+            level.tiles[j + i * level.width].coin.setTexture(level.txCoin);
 
             switch (level.map_string.at(j + i * level.width)) {
             case '1':
@@ -411,7 +491,16 @@ void initLevel(Level& level) {
                 level.tiles[j + i * level.width].sprite.setTextureRect(IntRect(16 + 64, 16 + 64, 16, 16));
                 level.tiles[j + i * level.width].isSolid = true;
                 break;
-            case 'C': break; // coin here, texture is "./assets/level/gem.png"
+            case 'F':
+                level.tiles[j + i * level.width].sprite.setTexture(level.feather);
+                level.tiles[j + i * level.width].sprite.setTextureRect(IntRect(0,0, 16, 16));
+                level.tiles[j + i * level.width].isPowerup = true;
+                break;
+            case 'C':
+                level.tiles[j + i * level.width].coin.setTextureRect(IntRect(0, 0, 16, 16));
+                level.tiles[j + i * level.width].coin.setScale(2, 2);
+                level.tiles[j + i * level.width].isCoin = true;
+                break; 
             default: break;
             }
 
@@ -433,19 +522,43 @@ void loadLevelFile(Level& level) {
 }
 
 void updateLevel(Game& game) {
+    game.map.score_text.setPosition(round(game.camera.getCenter().x-100), 0);
+    game.map.score_text.setString("SCORE: " + std::to_string(game.player.score));
+
+    if (game.map.timer.getElapsedTime().asSeconds() >= 1) {
+        game.map.countdown_timer--;
+        game.map.timer_text.setString("TIME: " + std::to_string(game.map.countdown_timer));
+        game.map.timer.restart();
+        if (game.map.countdown_timer <= 0)
+            game.window.close();
+    }
+
+    game.map.timer_text.setPosition(round(game.camera.getCenter().x + 200), 0);
+
+
     game.map.background.setPosition(game.camera.getCenter());
+
+
 }
 
 void drawLevel(Game& game) {
 
     game.window.draw(game.map.background);
+    game.window.draw(game.map.timer_text);
+    game.window.draw(game.map.score_text);
+
     for (int i = 0; i < game.map.height; i++) {
         for (int j = 0; j < game.map.width; j++) {
 
             Tile &currentTile = game.map.tiles[j + i * game.map.width];
-            if (currentTile.isSolid || currentTile.isCoin) { //make sure sprite isn't empty
+            if (currentTile.isSolid || currentTile.isPowerup) { //make sure sprite isn't empty
                 game.window.draw(game.map.tiles[j + i * game.map.width].sprite);
             }
+
+            if (currentTile.isCoin) {
+                game.window.draw(game.map.tiles[j + i * game.map.width].coin);
+            }
+                
 
         }
     }
